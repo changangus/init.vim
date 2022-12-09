587395a53e01e7d60e4bccfce12bb8f81d3b716d
Angus Chang <anguschang@achang-mbp16.myfiosgateway.com>
2022-12-09 12:02:45 -0500

    feat: git signs package added

--- init.lua
+++ init.lua
@@ -80,8 +80,6 @@ packer.startup(function(use)
   use { "nvim-telescope/telescope-file-browser.nvim" }
   -- Nerdtree
   use 'preservim/nerdtree'
-  -- Git Gutter
-  use 'airblade/vim-gitgutter'
   -- autoclose
   use 'm4xshen/autoclose.nvim'
   -- Themes:
@@ -146,6 +144,11 @@ packer.startup(function(use)
   use('MunifTanjim/prettier.nvim')
   -- nvim dap
   use 'mfussenegger/nvim-dap'
+  -- git signs
+  use {
+    'lewis6991/gitsigns.nvim',
+    -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
+  }
 end)
 
 -- LSP Config:
@@ -450,3 +453,44 @@ prettier.setup({
   },
 })
 
+-- git signs
+require('gitsigns').setup {
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, { expr = true })
+
+    map('n', '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, { expr = true })
+
+    -- Actions
+    map({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+    map({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+    map('n', '<leader>hS', gs.stage_buffer)
+    map('n', '<leader>hu', gs.undo_stage_hunk)
+    map('n', '<leader>hR', gs.reset_buffer)
+    map('n', '<leader>hp', gs.preview_hunk)
+    map('n', '<leader>hb', function() gs.blame_line { full = true } end)
+    map('n', '<leader>tb', gs.toggle_current_line_blame)
+    map('n', '<leader>hd', gs.diffthis)
+    map('n', '<leader>hD', function() gs.diffthis('~') end)
+    map('n', '<leader>td', gs.toggle_deleted)
+
+    -- Text object
+    map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  end
+}
