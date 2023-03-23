9bb7f7a0dfc2d9bc879f925c1a44d52304d914c9
Angus Chang <changangus2@gmail.com>
2023-03-22 22:31:35 -0400

    feat: adjust lualine styles

--- init.lua
+++ init.lua
@@ -438,8 +438,8 @@ require('lualine').setup {
   options = { theme = 'catppuccin' },
   sections = {
     lualine_a = { 'mode' },
-    lualine_b = { 'filename' },
-    lualine_c = { 'branch' },
+    lualine_b = { 'branch' },
+    lualine_c = {{ 'filename', path = 1 }},
     lualine_y = {},
     lualine_z = {},
   }
